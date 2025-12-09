c3c0b06a601877146fae80ddbe8973a1e5e8ac90
Alexander Mann <alex.mann@wolt.com>
2025-12-09 10:02:09 +0200

    add notes

--- src/solutions/Day09.kt
+++ src/solutions/Day09.kt
@@ -16,8 +16,8 @@ private data class Point(val x: Long, val y: Long) {
 
 fun main() {
     fun calculateArea(a: Point, b: Point): Long {
-        val first = if (a.x >= b.x) { a.x - b.x + 1 } else { b.x - a.x + 1}
-        val second = if (a.y >= b.y) { a.y - b.y + 1 } else { b.y - a.y + 1 }
+        val first = if (a.x >= b.x) a.x - b.x + 1 else b.x - a.x + 1
+        val second = if (a.y >= b.y) a.y - b.y + 1 else b.y - a.y + 1
         return first * second
     }
 
@@ -31,9 +31,15 @@ fun main() {
 
         // the twist now is that we need to check that all points within the area are 'in bounds'
         // first we need to determine where the boundaries are
-        // create a list of 'in bounds' points and iterate through all points to draw lines between them, adding points
-        // as we go.
-        // Then, for each area,
+        // create a list of 'in bounds' points and iterate through all points to draw lines between them, adding points as we go.
+        // We only need to check two direction from each point, as we are only interested in the points between them.
+        // we need to check if point B is either north or south and west or east from point A by diff-ing the x-y coords.
+        // we then start by, for example, North from point A, checking the in-bounds list for each point as we decrement 'x'
+        // if we hit an out-of-bounds point, we must perform an in-bounds check.
+        // We iterate north, south, east, and west and if we hit a boundary on all sides, we are in-bounds and add the point to the list
+        // If we find that have indeed gone out-of-bounds, we stop immediately and can dismiss this as a valid area
+        // Optimisation idea: binary search through the list to minimise the number of checks...?
+
 
         println(areas.size)
         println("Time taken: ${System.currentTimeMillis() - startTime} ms.")
